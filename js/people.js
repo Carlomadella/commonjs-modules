a0fafa56e1e98506241a6e50a5f4b27501e777ef
Carlomadella <madella871@gmail.com>
2025-04-14 16:52:32 +0200

    [UPD] Esercizio 3 - completato punto 3b

--- js/people.js
+++ js/people.js
@@ -4,3 +4,6 @@
 //     3c. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 
 const names = require("./names.js");
+
+const hobbies = require("./hobbies.js");
+
