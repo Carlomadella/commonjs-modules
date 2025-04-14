3b4bbb3b4420294c0d7f059e7a8e3e8c266b681d
Carlomadella <madella871@gmail.com>
2025-04-14 16:46:51 +0200

    [UPD] Esercizio 2 - completato punto 2a

--- js/hobbies.js
+++ js/hobbies.js
@@ -1,3 +1,10 @@
 // 2 - hobbies.js
 //     2a. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
 //     2b. Esporta la funzione dal file.
+
+let hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+    const myHobbies = [hobbyOne, hobbyTwo, hobbyThree];
+    return myHobbies;
+}
+
+// console.log(hobbies("Football", "Basketball", "LOL"));
