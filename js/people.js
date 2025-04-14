b37faf56312b340614e36885ac6fe9ca096ba040
Carlomadella <madella871@gmail.com>
2025-04-14 16:51:06 +0200

    [UPD] Esercizio 3 - completato punto 3a

--- js/people.js
+++ js/people.js
@@ -2,3 +2,5 @@
 //     3a. Importa la tua funzione da names.js
 //     3b. Importa la tua funzione da hobbies.js
 //     3c. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+const names = require("./names.js");
